0bbad9fc041653c66b50ed6c7a654c8678b4035c
Temma <temmavn@gmail.com>
2026-05-13 09:06:55 +0700

    Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
+import React from 'react'
+import Button from './components/Button'
+import { ArrowLeft, Search } from 'lucide-react'
+import PageHeader from './layouts/PageHeader'
+import MainHeader from './layouts/MainHeader'
+import {useMediaQuery} from './mystate/useMediaQuery'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const isHideMainHeader = useMediaQuery('(min-width: 1250px)');
   return (
-    <div className="">
-      App
+    <div className='bg-white'>
+    <PageHeader/>
+    {isHideMainHeader && <MainHeader></MainHeader>}
     </div>
   )
 }
 
-export default App
+export default App;
